8b7acaeec4d20e4baadcf015590325aff1900dcf
K3NT0Z4K1 <kentryanpagongpong@gmail.com>
2026-03-19 21:20:31 +0800

    Personally changed some href for the style based on my understanding

--- auth/signup.php
+++ auth/signup.php
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EcoProtean - Sign Up</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="signup-container">
